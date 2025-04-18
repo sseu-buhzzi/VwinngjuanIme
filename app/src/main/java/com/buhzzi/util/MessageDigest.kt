7e23f5c78da5031bc99e728f9242e7223ea36474
xeu <xeu@debian>
2025-04-18 22:30:39 +0800

    文字版顯示檔校驗和。

---
+++ app/src/main/java/com/buhzzi/util/MessageDigest.kt
@@ -0,0 +1,21 @@
+package com.buhzzi.util
+
+import java.math.BigInteger
+import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
+import java.nio.file.Path
+import java.security.MessageDigest
+
+fun Path.getSha256Sum() = MessageDigest.getInstance("SHA-256").run {
+	val buffer = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE)
+	FileChannel.open(this@getSha256Sum).use { `in` ->
+		while (`in`.read(buffer) != -0x1) {
+			buffer.flip()
+			update(buffer)
+			buffer.clear()
+		}
+	}
+	digest()
+}
+
+fun ByteArray.bigIntegerToString() = BigInteger(0x1, this).toString(0x10)
