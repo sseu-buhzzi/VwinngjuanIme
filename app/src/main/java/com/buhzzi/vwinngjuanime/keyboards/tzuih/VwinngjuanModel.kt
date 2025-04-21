55b61c90b9c51a29e6df64e756f6fd0e9c770d0e
xeu <xeu@debian>
2025-04-22 00:10:23 +0800

    新增選字欄、組皆附、居中。

--- app/src/main/java/com/buhzzi/vwinngjuanime/keyboards/tzuih/VwinngjuanModel.kt
+++ app/src/main/java/com/buhzzi/vwinngjuanime/keyboards/tzuih/VwinngjuanModel.kt
@@ -2,6 +2,7 @@ package com.buhzzi.vwinngjuanime.keyboards.tzuih
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.util.fastFlatMap
@@ -209,7 +210,7 @@ internal class ComposingVwinStack(
 ) {
 	private val vwinList = mutableListOf<VwinInfo>()
 
-	val tzhuList = mutableListOf<Pair<TzhuNode, Int>>()
+	val tzhuList = mutableStateListOf<Pair<TzhuNode, Int>>()
 
 	val notEmpty
 		get() = vwinList.isNotEmpty()
